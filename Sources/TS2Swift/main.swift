7cb0c4f5704aea1fc18802b46eb8fb60b96c5efa
Roy Cao <roy.cao@shopee.com>
2021-11-01 19:52:06 +0800

    Fix TSParserListener

--- Sources/TS2Swift/main.swift
+++ Sources/TS2Swift/main.swift
@@ -1,5 +1,6 @@
 import Antlr4
 import TSParserAntlr
+import TSParser
 
 do {
     let chars = try ANTLRFileStream("/Users/roycao/Desktop/tsTest/examples/Function.ts")
@@ -7,7 +8,22 @@ do {
     let tokens = CommonTokenStream(lexer)
     let parser = try TypeScriptParser(tokens)
     parser.setBuildParseTree(true)
-    try parser.expressionSequence()
+
+    let root = try parser.program()
+    
+//    let root = try parser.initializer()
+//    try parser.expressionSequence()
+//    try parser.classDeclaration()
+    
+//    let root = try parser.functionDeclaration()
+//
+    let listener = TSParserListener()
+
+    let walker = ParseTreeWalker()
+    try walker.walk(listener, root)
+    
+    print("------")
+    
 } catch {
     print(error)
 }
--- Sources/TSParser/TSParserListener.swift
+++ Sources/TSParser/TSParserListener.swift
@@ -1,6 +1,26 @@
 import Antlr4
 import TSParserAntlr
 
-class TSParserListener: TypeScriptParserBaseListener {
+public class TSParserListener: TypeScriptParserBaseListener {
+    
+    public override func enterClassDeclaration(_ ctx: TypeScriptParser.ClassDeclarationContext) {
+        let classDeclName = ctx.description
+        print(classDeclName)
+    }
+    
+    public override func enterFunctionDeclaration(_ ctx: TypeScriptParser.FunctionDeclarationContext) {
+        let funcDeclName = ctx.Identifier()?.getSymbol()?.getText()
+        let parameter = ctx.callSignature()?.parameterList()?.parameter()
+    
+        print(funcDeclName)
+        for a in parameter ?? [] {
+            print(a.requiredParameter()?.identifierOrPattern()?.getText())
+        }
+        
+    }
+    
+    public override func enterFunctionBody(_ ctx: TypeScriptParser.FunctionBodyContext) {
+        
+    }
     
 }
